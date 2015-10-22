e8cd31ed1877d6a448851d7808ef12473e77d130
hardywen <375480616@qq.com>
2015-10-22 14:37:50 +0800

    update

--- src/SSOServiceProvider.php
+++ src/SSOServiceProvider.php
@@ -3,15 +3,8 @@
 namespace Hardywen\SSOClient;
 
 
-<<<<<<< HEAD
 use Hardywen\SSOClient\Middleware\CORSResponse;
 use Hardywen\SSOClient\Middleware\SSOAuthenticate;
-=======
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Response;
->>>>>>> 63c8fbe112da9cdaf48af1a33c7e3b3d02015128
 use Illuminate\Support\ServiceProvider;
 
 class SSOServiceProvider extends ServiceProvider
@@ -19,7 +12,13 @@ class SSOServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->package('hardywen/sso-client');
+        $config = realpath(__DIR__ . '/../config/sso.php');
+
+        $this->mergeConfigFrom($config, 'sso');
+
+        $this->publishes([
+            $config => config_path('sso.php')
+        ]);
     }
 
     /**
@@ -31,7 +30,6 @@ class SSOServiceProvider extends ServiceProvider
     {
         $router = $this->app['router'];
 
-<<<<<<< HEAD
         $router->middleware('auth', SSOAuthenticate::class);
         $router->middleware('cors', CORSResponse::class);
 
@@ -39,50 +37,6 @@ class SSOServiceProvider extends ServiceProvider
         $router->get('sso/logout', ['as'=>'sso.logout','uses'=>'Hardywen\SSO\SSOController@logout']);
         $router->get('sso/clear', ['as'=>'sso.clear','middleware'=>'cors','uses'=>'Hardywen\SSO\SSOController@clear']);
 
-=======
-        $config = $this->app->config->get('sso-client::config');
-
-        $router->get('sso/login', function () use ($request,$config) {
-
-            $sign = $request->get('sign');
-            $time = $request->get('time');
-            $ssoToken = $request->get('sso_token');
-
-            $encryptKey = $config['sso_token_encrypt_key'];
-
-            if ($sign && $time && $ssoToken) {
-                if ($sign == md5($encryptKey . $time . $ssoToken)) {
-                    $merchant = $this->createModel();
-
-                    $merchant = $merchant->where('sso_token', $ssoToken)->where('sso_token_created_at', '>=',
-                        Carbon::now()->subMinute())->first();
-
-                    if ($merchant) {
-                        Auth::login($merchant);
-
-                        return redirect()->intended();
-                    }
-                }
-            }
-        });
-
-        $router->get('sso/clear', function () {
-            Auth::logout();
-
-            return Response::make('', 204);
-        });
-
-        $router->filter('auth', function() use ($request,$config){
-            if (Auth::guest()) {
-
-                if ($request->ajax()) {
-                    return Response::make('Unauthorized.', 401);
-                } else {
-                    return Redirect::guest($config['sso.sso_login_url'] . '?redirect_url=' . urlencode(url('sso/login')));
-                }
-            }
-        });
->>>>>>> 63c8fbe112da9cdaf48af1a33c7e3b3d02015128
     }
 
     /**
@@ -92,19 +46,8 @@ class SSOServiceProvider extends ServiceProvider
      */
     public function createModel()
     {
-        $class = '\\' . ltrim($this->app['config']->get('auth.model'), '\\');
+        $class = '\\' . ltrim($this->app['config']['auth.model'], '\\');
 
         return new $class;
-    }
-
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array('sso-client');
     }
 }
